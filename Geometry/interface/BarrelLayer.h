80719a4b20b782582faa5c72c06bc1724cfbf57b
Simon Kurz <Simon.Kurz@desy.de>
2016-05-18 15:46:41 +0200

    created new files

---
+++ Geometry/interface/BarrelLayer.h
@@ -0,0 +1,14 @@
+#ifndef FASTSIM_BARRELLAYER_H
+#define FASTSIM_BARRELLAYER_H
+
+namespace fastsim{
+
+    class BarrelLayer
+    {
+	
+	
+    };
+
+}
+
+#endif
---
+++ Geometry/interface/ForwardLayer.h
@@ -0,0 +1,14 @@
+#ifndef FASTSIM_FORWARDLAYER_H
+#define FASTSIM_FORWARDLAYER_H
+
+namespace fastsim{
+
+    class ForwardLayer
+    {
+	
+	
+    };
+
+}
+
+#endif
