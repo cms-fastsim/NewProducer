8027f1a2832fa390c9e2e22ed287a4b8a6cadbcb
Simon Kurz <Simon.Kurz@desy.de>
2016-11-02 21:20:13 +0100

    added Taylor expansion

--- Propagation/src/HelixTrajectory.cc
+++ Propagation/src/HelixTrajectory.cc
@@ -76,79 +76,139 @@ double fastsim::HelixTrajectory::nextCrossingTimeC(const BarrelLayer & layer) co
     // c = E^2 - G^2
     //
 
-    double E = centerX_*centerX_ + centerY_*centerY_ + radius_*radius_ - layer.getRadius()*layer.getRadius();
-    double F = 2*centerY_*radius_;
-    double G = 2*centerX_*radius_;
+    if(radius_ < 50000){
+        double E = centerX_*centerX_ + centerY_*centerY_ + radius_*radius_ - layer.getRadius()*layer.getRadius();
+        double F = 2*centerY_*radius_;
+        double G = 2*centerX_*radius_;
 
-    double a = F*F + G*G;
-    double b = 2*E*F;
-    double c = E*E - G*G;
-    
-    double delta = b*b - 4*a*c;
-    // case of no solution
-    if(delta < 0)
-    {   
-    	// Should not be reached
-    	throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "should not be reached";
-        return -1.;
-    }
+        double a = F*F + G*G;
+        double b = 2*E*F;
+        double c = E*E - G*G;
 
-    double sqrtDelta = sqrt(delta);
-    double phi1 = std::asin((-b - sqrtDelta) / (2.*a));
-    double phi2 = std::asin((-b + sqrtDelta) / (2.*a));
-    // asin is ambiguous, make sure to have the right solution
-    if(std::abs(layer.getRadius() - sqrt((centerX_ + radius_*std::cos(phi1))*(centerX_ + radius_*std::cos(phi1)) + (centerY_ + radius_*std::sin(phi1))*(centerY_ + radius_*std::sin(phi1)))) > 1e-3){
-        phi1 = - phi1 + M_PI;
-        //std::cout<<"-> fixing phi1"<<std::endl;
-    }
-    if(std::abs(layer.getRadius() - sqrt((centerX_ + radius_*std::cos(phi2))*(centerX_ + radius_*std::cos(phi2)) + (centerY_ + radius_*std::sin(phi2))*(centerY_ + radius_*std::sin(phi2)))) > 1e-3){
-        phi2 = - phi2 + M_PI;
-        //std::cout<<"-> fixing phi2"<<std::endl;
-    }
+        double delta = b*b - 4*a*c;
+        // case of no solution
+        if(delta < 0)
+        {   
+        	// Should not be reached
+        	throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "should not be reached";
+            return -1.;
+        }
 
-    if(phi1 < 0){
-        phi1 += 2. * M_PI;
-    }
-    if(phi2 < 0){
-        phi2 += 2. * M_PI;
-    }
+        double sqrtDelta = sqrt(delta);
+        double phi1 = std::asin((-b - sqrtDelta) / (2.*a));
+        double phi2 = std::asin((-b + sqrtDelta) / (2.*a));
+        // asin is ambiguous, make sure to have the right solution
+        if(std::abs(layer.getRadius() - sqrt((centerX_ + radius_*std::cos(phi1))*(centerX_ + radius_*std::cos(phi1)) + (centerY_ + radius_*std::sin(phi1))*(centerY_ + radius_*std::sin(phi1)))) > 1e-3){
+            phi1 = - phi1 + M_PI;
+            //std::cout<<"-> fixing phi1"<<std::endl;
+        }
+        if(std::abs(layer.getRadius() - sqrt((centerX_ + radius_*std::cos(phi2))*(centerX_ + radius_*std::cos(phi2)) + (centerY_ + radius_*std::sin(phi2))*(centerY_ + radius_*std::sin(phi2)))) > 1e-3){
+            phi2 = - phi2 + M_PI;
+            //std::cout<<"-> fixing phi2"<<std::endl;
+        }
 
-    if(std::abs(layer.getRadius() - sqrt((centerX_ + radius_*std::cos(phi1))*(centerX_ + radius_*std::cos(phi1)) + (centerY_ + radius_*std::sin(phi1))*(centerY_ + radius_*std::sin(phi1)))) > 1e-3){
-        throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "not able to calculate phi1 of intersection";
-    }
-    if(std::abs(layer.getRadius() - sqrt((centerX_ + radius_*std::cos(phi2))*(centerX_ + radius_*std::cos(phi2)) + (centerY_ + radius_*std::sin(phi2))*(centerY_ + radius_*std::sin(phi2)))) > 1e-3){
-        throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "not able to calculate phi2 of intersection";
+        if(phi1 < 0){
+            phi1 += 2. * M_PI;
+        }
+        if(phi2 < 0){
+            phi2 += 2. * M_PI;
+        }
+
+        if(std::abs(layer.getRadius() - sqrt((centerX_ + radius_*std::cos(phi1))*(centerX_ + radius_*std::cos(phi1)) + (centerY_ + radius_*std::sin(phi1))*(centerY_ + radius_*std::sin(phi1)))) > 1e-3){
+            throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "not able to calculate phi1 of intersection";
+        }
+        if(std::abs(layer.getRadius() - sqrt((centerX_ + radius_*std::cos(phi2))*(centerX_ + radius_*std::cos(phi2)) + (centerY_ + radius_*std::sin(phi2))*(centerY_ + radius_*std::sin(phi2)))) > 1e-3){
+            throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "not able to calculate phi2 of intersection";
+        }    
+
+        // find the corresponding times
+        // make sure they are positive
+        double t1 = (phi1 - phi_)/phiSpeed_;
+        while(t1 < 0)
+        {
+           t1 += 2*M_PI/std::abs(phiSpeed_);
+        }
+        double t2 = (phi2 - phi_)/phiSpeed_;
+        while(t2 < 0)
+        {
+           t2 += 2*M_PI/std::abs(phiSpeed_);
+        }
+
+        // if the particle is already on the layer,
+        // we need to make sure the 2nd solution is picked.
+        if(std::abs(phi1 - phi_)*radius_ < 1e-3){
+            return t2*speedOfLight_;
+        }
+        if(std::abs(phi2 - phi_)*radius_ < 1e-3){
+            return t1*speedOfLight_;
+        }
+
+        if(std::abs(phi1 - phi_)*radius_ < 1e-3 && std::abs(phi2 - phi_)*radius_ < 1e-3){
+            throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "should not happen. boundaries too loose!";
+        }
+
+        // not sure if we should stick to this t*c strategy...
+        return std::min(t1,t2)*speedOfLight_;
+
+    }else{
+        // Use Taylor approximation for small deltaPhi: sin(deltaPhi)=deltaPhi, cos(deltaPhi)=1
+        // x = x_c + R_H * cos(phi0 + deltaPhi)
+        //   = x_c + R_H * (cos(phi0) * cos(deltaPhi) - sin(phi0) * sin(deltaPhi))
+        //   = x_c + R_H * (cos(phi0) * 1             - sin(phi0) * deltaPhi)
+        //   = x_C + R_H * cos(phi0) - R_H * sin(phi0) * deltaPhi
+        // Similar (using sin(a+b) = sin(a)*cos(b) + cos(a)*sin(b))
+        // y = y_C + R_H * sin(phi0) + R_H * cos(phi0) * deltaPhi
+
+        // Plugging into R_L^2 = x^2 + y^2
+        // Leads to a quadratic equation with:
+
+        double c = (centerX_ + radius_ * std::cos(phi_))*(centerX_ + radius_ * std::cos(phi_)) + (centerY_ + radius_ * std::sin(phi_))*(centerY_ + radius_ * std::sin(phi_)) - layer.getRadius()*layer.getRadius();
+        double b = 2 * radius_ * (centerY_ * std::cos(phi_) - centerX_ * std::sin(phi_));
+        double a = radius_ * radius_;
+
+        double delta = b*b - 4*a*c;
+        // case of no solution
+        if(delta < 0)
+        {   
+            // Should not be reached
+            throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "should not be reached";
+            return -1.;
+        }
+
+        double sqrtDelta = sqrt(delta);
+        double delPhi1 = std::asin((-b - sqrtDelta) / (2.*a));
+        double delPhi2 = std::asin((-b + sqrtDelta) / (2.*a));
+
+        // Only one solution should be valid in most cases (Tayler expension only for small delPhi)
+        // If particle already on layer return -1 (unless second solution also very small):
+        double delPhi;
+        bool twoSolutions = false;
+        if(phiSpeed_ > 0){
+            if(delPhi1 > 0 && delPhi2 > 0){
+                delPhi = std::min(delPhi1, delPhi2);
+                twoSolutions = true;
+            }
+            else if(delPhi1 > 0) delPhi = delPhi1;
+            else delPhi = delPhi2;
+        }else{
+            if(delPhi1 < 0 && delPhi2 < 0){
+                delPhi = std::max(delPhi1, delPhi2);
+                twoSolutions = true;
+            }
+            else if(delPhi1 < 0) delPhi = delPhi1;
+            else delPhi = delPhi2;
+        }
+
+        if(std::abs(delPhi)*radius_ < 1e-3){
+            if(twoSolutions){
+                if(delPhi == delPhi1 && std::abs(delPhi2) < 1e-2) return delPhi2 / phiSpeed_ * speedOfLight_;
+                else if(delPhi == delPhi2 && std::abs(delPhi1) < 1e-2) return delPhi1 / phiSpeed_ * speedOfLight_;
+            }
+            return -1;
+        }
+        
+        return delPhi / phiSpeed_ * speedOfLight_;
     }    
-
-    // find the corresponding times
-    // make sure they are positive
-    double t1 = (phi1 - phi_)/phiSpeed_;
-    while(t1 < 0)
-    {
-	   t1 += 2*M_PI/std::abs(phiSpeed_);
-    }
-    double t2 = (phi2 - phi_)/phiSpeed_;
-    while(t2 < 0)
-    {
-	   t2 += 2*M_PI/std::abs(phiSpeed_);
-    }
-
-    // if the particle is already on the layer,
-    // we need to make sure the 2nd solution is picked.
-    if(std::abs(phi1 - phi_)*radius_ < 1e-3){
-        return t2*speedOfLight_;
-    }
-    if(std::abs(phi2 - phi_)*radius_ < 1e-3){
-        return t1*speedOfLight_;
-    }
-
-    if(std::abs(phi1 - phi_)*radius_ < 1e-3 && std::abs(phi2 - phi_)*radius_ < 1e-3){
-        throw cms::Exception("fastsim::HelixTrajectory::nextCrossingTimeC") << "should not happen. boundaries too loose!";
-    }
-
-    // not sure if we should stick to this t*c strategy...
-    return std::min(t1,t2)*speedOfLight_;
-    
 }
 
 void fastsim::HelixTrajectory::move(double deltaTimeC)
